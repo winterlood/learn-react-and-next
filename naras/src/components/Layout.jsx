58c425ea7d6ded2c427f173ba4a6b47042afe55b
winterlood <king199777@gmail.com>
2023-06-21 13:40:58 +0900

    ✨로고 클릭시 인덱스로 이동 기능 추가

--- naras/src/components/Layout.jsx
+++ naras/src/components/Layout.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import style from "./Layout.module.css";
 export default function Layout({ children }) {
+  const nav = useNavigate();
+
+  const onClickLogo = () => {
+    nav("/");
+  };
+
   return (
     <div className={style.container}>
-      <header>NARAS</header>
+      <header>
+        <div className={style.logo} onClick={onClickLogo}>
+          🌏 NARAS
+        </div>
+      </header>
       <main>{children}</main>
     </div>
   );
